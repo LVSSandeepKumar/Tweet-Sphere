e79c58081578b29557bc4a82802aeb1aee9cedc8
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-07-20 15:20:58 +0530

    Post Routes and Controllers

---
+++ backend/routes/post.routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {protectRoute} from "../middleware/protectRoute.js";
+import { commentOnPost, createPost, deletePost, getAllPosts, getFollowingPosts, getLikedPosts, getUserPosts, likeUnlikePost } from "../controllers/post.controller.js";
+
+const router = Router();
+
+router.get("/all", protectRoute, getAllPosts);
+router.get("/liked/:id", protectRoute, getLikedPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
+router.post("/create", protectRoute, createPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.delete("/:id", protectRoute, deletePost);
+
+export default router;
