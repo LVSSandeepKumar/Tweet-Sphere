ffc099db0c0eceb5889bc4b591340c625dd504f8
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-07-22 13:34:53 +0530

    CREATE posts

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 
-app.use(express.json());
+app.use(express.json({limit: "5mb"}));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
